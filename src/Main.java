04220c3d93d29c01cffd7f9a0dfc0044ad60b84c
JulianA <jalejandroalgarra@ucundinamarca.edu.co>
2025-08-28 16:49:53 -0500

    Resumen comandos git

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,16 @@ public class Main {
         System.out.println("Hello 1");
         System.out.println("Hello 1");
         System.out.println("Hello 1");
-
+        System.out.println("Comandos git");
+        System.out.println("git status - estado rama");
+        System.out.println("git branch - ver las ramas");
+        System.out.println("git add . - agrupar archivos");
+        System.out.println("git push origin - subir cambios");
+        System.out.println("git commit -m - guardar cambios local");
+        System.out.println("git config -- list - listar configuracion");
+        System.out.println("git log - ver todos los commit");
+        System.out.println("git clone - clonar proyecto");
+        System.out.println("git fetch - ver ramas remotas");
+        System.out.println("git switch - crear y posicionarme en la rama");
     }
 }
